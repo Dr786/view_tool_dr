df538f6ee5664bc6b579a15ba71c20dd4187787b
Dr <shakil.akbar@yahoo.com>
2020-08-07 16:16:05 +0500

    Initial Commit

---
+++ lib/view_tool_dr.rb
@@ -0,0 +1,7 @@
+require "view_tool_dr/version"
+require "view_tool_dr/renderer"
+
+module ViewToolDr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/view_tool_dr/version.rb
@@ -0,0 +1,3 @@
+module ViewToolDr
+  VERSION = "0.1.0"
+end
